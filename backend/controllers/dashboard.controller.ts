e284d4ad6f7bd5be1213589b26f582606bc995e9
Jv Bialen <162081127+meliodaff@users.noreply.github.com>
2025-11-05 16:59:39 +0800

    done dashboard endpoints

--- backend/controllers/dashboard.controller.ts
+++ backend/controllers/dashboard.controller.ts
@@ -27,6 +27,78 @@ const currentInventory = [
   },
 ];
 
+const procurement = [
+  {
+    orderId: 544125,
+    supplier: "ABC Supplies Co.",
+    items: 15,
+    totalValue: 45000.0,
+    status: "Delivered",
+    date: "2025-09-10",
+  },
+  {
+    supplier: "Hotel Essentials Ltd.",
+    items: 8,
+    totalValue: 28500.0,
+    status: "In Transit",
+    date: "2025-09-11",
+  },
+  {
+    supplier: "Quality Linens Inc.",
+    items: 20,
+    totalValue: 67800.0,
+    status: "Pending",
+    date: "2025-09-12",
+  },
+];
+
+type Type = "negative" | "positive";
+
+type DashboardSummary = {
+  title: string;
+  value: number;
+  change: string;
+  type: Type;
+};
+
+const dashboardSummary: DashboardSummary[] = [
+  {
+    title: "Total Items",
+    value: 1974,
+    change: "+15% from last month",
+    type: "positive",
+  },
+  {
+    title: "Low Stock Items",
+    value: 56,
+    change: "Requires Attention",
+    type: "negative",
+  },
+  {
+    title: "Pending Orders",
+    value: 99,
+    change: "-5% from last month",
+    type: "negative",
+  },
+  {
+    title: "Total Value",
+    value: 867,
+    change: "+8% from last month",
+    type: "positive",
+  },
+];
+
 export const getCurrentInventory = (req: Request, res: Response) => {
-  res.json(currentInventory);
+  res.status(200).json(currentInventory);
+};
+
+export const getRecentProcurementActivities = (req: Request, res: Response) => {
+  res.status(200).json(procurement);
+};
+
+export const getDashboardSummary = (req: Request, res: Response) => {
+  if (dashboardSummary.length <= 0) {
+    res.status(500).json({ message: "Missing Dashboard Summary" });
+  }
+  res.status(200).json(dashboardSummary);
 };
--- backend/routes/dashboard.ts
+++ backend/routes/dashboard.ts
@@ -4,5 +4,10 @@ import * as indexController from "../controllers/dashboard.controller.js";
 const router = express.Router();
 
 router.get("/get-current-inventory", indexController.getCurrentInventory);
+router.get(
+  "/get-recent-procurement",
+  indexController.getRecentProcurementActivities
+);
+router.get("/get-dashboard-summary", indexController.getDashboardSummary);
 
 export default router;
